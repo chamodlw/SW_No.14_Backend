397f218e992ae3d7f48ce30a695ac6df0bdaf521
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-04-29 23:02:26 +0530

    2024.04.29
    Initial point

---
+++ src/controllers/controller(RB_LA).js
@@ -0,0 +1,32 @@
+const { request } = require('http');
+const { response } = require('./app');
+const User = require('./model');
+
+const getUsers =(req,res,next)=>{
+    User.find()
+    .then(response=>{
+        res.json({response})
+    })
+    .catch(error=>{
+        res.json({message:error})
+    })
+};
+
+
+
+
+const UpdateUser = (req,res,next)=>{
+    const { id,name} = req.body;
+    User.updateOne({id:id},{$set:{name:name}},)
+    .then(response=>{
+        res.json({response})
+    })
+    .catch(error=>{
+        res.json({error})
+    });
+
+
+    }
+
+    exports.getUsers= getUsers;
+    exports.UpdateUser=UpdateUser;

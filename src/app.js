23cbc6c8f755bca475ed272c2d997d3eedfff1ae
chamodlw <chamodweerasinghe@gmail.com>
2024-06-08 23:04:19 +0530

    8/6-11.04

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-//app.jsc
+//app.js
 const express = require('express'); //Importing the express framework by requiring the 'express'module.
 const app = express();  //Creates an instance of the Express application.
 const cors = require('cors');

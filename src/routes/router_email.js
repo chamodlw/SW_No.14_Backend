966e13dc514b1b7263aa653ab425e43239653afd
Kavini-hub <141908913+Kavini-hub@users.noreply.github.com>
2024-06-19 09:50:23 +0530

    6/19-2

--- src/routes/router_email.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { approveRecommendation } = require('../controllers/controller_email');
-const { authenticateJWT, authorizeRoles } = require('../controllers/controller_email');
-
-// Approve recommendation route
-router.get('/recommendations/:id/approve', authenticateJWT, authorizeRoles('admin', 'doctor'), approveRecommendation);
-
-module.exports = router;

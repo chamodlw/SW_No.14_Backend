22541019221e3a9644989a7819c7eec85583ca05
TheodaVidyani <theodahettiarachchi00@gmail.com>
2024-06-30 11:28:51 +0530

    0630

--- src/models/model_login.js
+++ src/models/model_login.js
@@ -11,18 +11,22 @@ const userSchema = new Schema({
     phonenumber: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['DOCTOR', 'PATIENT', 'ADMIN','LABOPERATOR', 'LABASSISTANT'], required: true }
+    role: { type: String, enum: ['DOCTOR', 'PATIENT', 'ADMIN', 'LABASSISTANT', 'LABOPERATOR'], required: true },
+    profilePic: { type: String },
+    verificationCode: { type: String },
+    verificationCodeExpires: { type: Date }
 });
 
+// Static method to authenticate user
 userSchema.statics.authenticate = async function(username, password) {
     try {
-        const user = await this.findOne({ username: username }).exec();
+        const user = await this.findOne({ username }).exec();
         if (!user) {
             console.log('No user found');
             return null;
         }
 
-        const isPasswordValid = await bcrypt.compare(password, user.password); //To check with the password we have in the DB
+        const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
             console.log('Invalid password');
             return null;
@@ -35,4 +39,3 @@ userSchema.statics.authenticate = async function(username, password) {
 };
 
 module.exports = mongoose.model('User', userSchema);
- 

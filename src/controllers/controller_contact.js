36e8917d66ef6cbd2be1f96b2241f9bb372baef8
Kavini-hub <141908913+Kavini-hub@users.noreply.github.com>
2024-07-09 23:04:23 +0530

    7/9

--- src/controllers/controller_contact.js
+++ src/controllers/controller_contact.js
@@ -1,23 +1,22 @@
 const Contact = require('../models/model_contact');
 
-
-// Function to handle saving recommendations
 const contact = (req, res) => {
-    const {  name,email,no,message } = req.body;
-    const newContact = new Contact({
-       
-        name:name,
-        email:email,
-        contact_No:no,
-        message:message,
-    });
+  const { name, email, contact_No, message, date } = req.body;
+  const newContact = new Contact({
+    name: name,
+    email: email,
+    contact_No: contact_No,
+    date: date,
+    feedback: message,
+  });
 
-    newContact.save()
-        .then(response => {
-            res.json({ response });
-        })
-        .catch(error => {
-            res.status(500).json({ error });
-        });
+  newContact.save()
+    .then(response => {
+      res.json({ response });
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 };
-module.exports = {contact };
+
+module.exports = { contact };
--- src/models/model_contact.js
+++ src/models/model_contact.js
@@ -1,14 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const mongoose=require('mongoose');
-const Schema =mongoose.Schema;
+const contactSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  contact_No: { type: String, required: true },
+  date: { type: Date, required: true },
+  feedback: { type: String, required: true },
+});
 
-const contactSchema =new Schema({
-    name:String,
-    email:String,
-    contact_No:String,
-    message:String,
-   
-})
-const Contact =mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model('Contact', contactSchema);
 
 module.exports = Contact;
